2fb10bf2a7c0074a4719fe19b946dbbfad7536d3
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-08-27 08:50:28 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import { Webview } from "@webview/webview";
+import { SizeHint, Webview } from "@webview/webview";
 
 const html = `
 <html>
-<body>
-<h1>Hello from deno v${Deno.version.deno}</h1>
-<button onclick="openFile('./src/index.ts').then(log);">Open</button>
-</body>
+  <head>
+    <style>
+    body {
+      background-color: gray;
+    }
+    </style>
+  </head>
+  <body>
+    <h1>Hello from deno v${Deno.version.deno}</h1>
+    <button onclick="openFile('./src/index.ts').then((text) => log(text));">Open</button>
+  </body>
 </html>
 `;
 
-const webview = new Webview(true);
+const webview = new Webview(true, {
+  hint: SizeHint.MIN,
+  width: 500,
+  height: 500
+});
 
 webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
 
@@ -19,4 +30,5 @@ webview.bind("openFile", (file) => {
 
 webview.bind("log", (...args) => console.log(...args));
 
+webview.title = 'Hello World!'
 webview.run();
